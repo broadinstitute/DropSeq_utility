e3186eb7020e45e3bff07e6cd9b04db655c2f874
bankapur <bankapur@delanatii.broadinstitute.org>
2018-01-23 12:57:39 -0500

    added R scripts

---
+++ collect_cell_barcodes.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+
+library(optparse)
+
+pArgs <- OptionParser( usage = "%prog input_reads_file input_ncells output_barcodes_file" )
+#pArgs <- add_option( pArgs, type="character", action="store", dest="str_input", metavar="Input_reads", help="Input reads file." )
+#pArgs <- add_option( pArgs, type="character", action="store", dest="str_input_ncells", metavar="Input_num_cells", help="Input number cells file." )
+#pArgs <- add_option( pArgs, type="character", action="store", dest="str_output", metavar="Output_barcodes", help="Output barcodes." )
+args <- parse_args( pArgs, positional_arguments = TRUE )
+
+data = read.table(file=args$args[1])
+Ncells = read.table(file=args$args[2])[1,1]
+barcodes = data$V2[1:Ncells]
+write.table(as.data.frame(barcodes), file=args$args[3], col.names=FALSE, row.names=FALSE,quote=FALSE)
