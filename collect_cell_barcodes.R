1a716812800a3624d8ec3fde1e067c2a67b86e60
jgould <jgould@broadinstitute.org>
2018-06-22 10:49:43 -0400

    read ncells as number

--- collect_cell_barcodes.R
+++ collect_cell_barcodes.R
@@ -9,6 +9,6 @@ pArgs <- OptionParser( usage = "%prog input_reads_file input_ncells output_barco
 args <- parse_args( pArgs, positional_arguments = TRUE )
 
 data = read.table(file=args$args[1])
-Ncells = read.table(file=args$args[2])[1,1]
+Ncells = as.numeric(args$args[2])
 barcodes = data$V2[1:Ncells]
 write.table(as.data.frame(barcodes), file=args$args[3], col.names=FALSE, row.names=FALSE,quote=FALSE)
