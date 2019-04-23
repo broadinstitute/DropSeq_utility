1a0ba22dab81692c882b3558284b814eb84f380b
jgould <jgould@broadinstitute.org>
2019-04-23 07:30:48 -0400

    take prefix from command line

--- add_fasta_prefix.py
+++ add_fasta_prefix.py
@@ -5,18 +5,19 @@ Add genome prefix to a fasta file.
 """
 
 import argparse
-import os
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Add a prefix for multiple species experiments")
-    parser.add_argument("--output", help="Output fasta", required=True)
+    parser.add_argument("--output", help="Output basename", required=True)
+    parser.add_argument("--prefix", required=True)
     parser.add_argument("input", help="fasta file", nargs='+')
     args = parser.parse_args()
-    with open(args.output, "w") as f:
-        for fasta_file in args.input:
-            prefix = os.path.basename(fasta_file)
-            dot_index = prefix.rfind('.')
-            prefix = prefix[0:dot_index]
+    fasta_files = args.input
+    prefix_list = args.prefix.split(',')
+    with open(args.output + '.fasta', "w") as f:
+        for i in range(len(fasta_files)):
+            fasta_file = fasta_files[i]
+            prefix = prefix_list[i]
             with open(fasta_file, "r") as reader:
                 for line in reader:
                     line = line.strip()
